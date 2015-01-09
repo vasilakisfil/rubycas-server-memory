91ab913fac146a04ac5ad60dfcccf0d84d477bc4
Filippos Vasilakis <vasilakisfil@gmail.com>
2015-01-09 12:18:34 +0100

    first commit

---
+++ lib/rubycas/server/memory.rb
@@ -0,0 +1,9 @@
+require "rubycas/server/memory/version"
+
+module Rubycas
+  module Server
+    module Memory
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubycas/server/memory/version.rb
@@ -0,0 +1,7 @@
+module Rubycas
+  module Server
+    module Memory
+      VERSION = "0.0.1"
+    end
+  end
+end
